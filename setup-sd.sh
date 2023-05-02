acb077ee179e271edde5fce06ec5e604fc3bb8b0
Avneesh Raghav <avneesh@mithyalabs.com>
2023-05-02 16:21:22 +0530

    setup sd script added

---
+++ setup-sd.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+echo "Setting up Automatic111"
+
+echo "Clone Automatic repo"
+
+#mkdir workspace
+#cd workspace
+#git clone https://github.com/mithyalabs/stable-diffusion-webui.git
+
+#cd ./stable-diffusion-webui
+
+#echo "Installing dependencies"
+
+# pip install -r ./requirements_versions.txt
+
+
+# echo "Downloading SD1.5 model"
+# wget https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt -P ./models/Stable-diffusion/
+
+echo "Installing controlnet extension"
+
+git clone https://github.com/Mikubill/sd-webui-controlnet.git ./extensions/sd-webui-controlnet
+
+echo "Downloading Canny model"
+wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_canny.pth -P ./models/ControlNet/
+wget https://huggingface.co/lllyasviel/ControlNet-v1-1/raw/main/control_v11p_sd15_canny.yaml -P ./models/ControlNet/
+
+# wget https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_normal.pth -P ./models/ControlNet/
+
+echo "Updating packages"
+
+sudo apt update -y
+
+echo "Installing nginx"
+
+sudo apt install nginx -y
+
+echo "Setting up firewall"
+sudo ufw allow 80
+sudo ufw allow 22
+sudo ufw enable
+
+sudo chown root /home/ubuntu/nginx.conf
+sudo mv /home/ubuntu/nginx.conf /etc/nginx/sites-available/default -f
+sudo service nginx reload
+
+# sudo sed -i 's/try_files $uri $uri\/ =404;/proxy_pass http:\/\/localhost:7860;/' /etc/nginx/sites-available/default
+
+echo "Setting up ebsynth utility"
+
+git clone -b feature/api https://github.com/mithyalabs/ebsynth_utility ./extensions/ebsynth_utility
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
+sudo dpkg -i cuda-keyring_1.0-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda
+
+sudo apt install nvidia-cuda-toolkit
+
+# Download ebsynth
+mkdir /home/ubuntu/bin
+wget https://pub-706bf4a189d94a6b8bfe844e4aaf385a.r2.dev/ebsynth_linux_cuda -O /home/ubuntu/bin/ebsynth
+chmod +x /home/ubuntu/bin/ebsynth
+
+
